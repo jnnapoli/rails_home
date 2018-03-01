b53017bb2f40276748806f4818f77b9ec121d489
jnnapoli <joey@jnnapoli.com>
2018-02-28 20:31:04 -0500

    (temporarily) Removed routes to blog_posts since it is not baseline implemented. moved home/indexHTML contents from applicationHTML to home/indexHTML. fixed header to link to / appropriately.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
-  resources :blog_posts
+
+
+
+  # REMOVE THE COMMENTING OUT OF THIS ONCE IM READY TO 'RELEASE' BLOG PORTION
+   # resources :blog_posts
+
+
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
